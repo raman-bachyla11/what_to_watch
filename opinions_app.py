560ee50dd5705ce8766c6dba00f2173850d2bdb5
Raman Bachyla <raman.bachyla@gmail.com>
2026-01-17 19:43:12 -0500

    project init, setup main page

--- opinions_app.py
+++ opinions_app.py
@@ -1,11 +1,39 @@
-from flask import Flask
+from datetime import datetime
+
+from random import randrange
+
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+app.json.ensure_ascii = False
+db = SQLAlchemy(app)
+
+
+class Opinion(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(128), nullable=False)
+    text = db.Column(db.Text, unique=True, nullable=False)
+    source = db.Column(db.String(256))
+    timestamp = db.Column(
+        db.DateTime,
+        index=True,
+        default=datetime.now
+    )
 
 
 @app.route('/')
 def index_view():
-    return 'Совсем скоро тут будет случайное мнение о фильме!'
+    quantity = Opinion.query.count()
+    if not quantity:
+        return 'В базе данных мнений о фильмах нет.'
+    offset_value = randrange(quantity)
+    opinion = Opinion.query.offset(offset_value).first()
+    return render_template('index.html', opinion=opinion)
+
 
 if __name__ == '__main__':
     app.run()
